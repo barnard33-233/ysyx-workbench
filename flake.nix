240b0f0da1622bd2dabbf34b360318a3f0baa002
barnard33-233 <htang0203@gmail.com>
2024-08-31 01:43:26 +0800

    chore: use nix-flake and direnv

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Flake for ysyx-workbench";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.05";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {self, nixpkgs, flake-utils, ...}@inputs :
+  flake-utils.lib.eachDefaultSystem (system: 
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs;[
+        gcc
+        gdb
+      ];
+    };
+
+    devShells.nemu = pkgs.mkShell {
+    # TODO: libreadline, libsdl2
+    };
+  });
+}
